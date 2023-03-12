dbe73c0192796f8157a5d43fa0f19cf9dcfea8d0
Arindam Saha <arindamsaha1507@gmail.com>
2023-03-12 20:43:39 +0000

    Added a basic test

---
+++ tests/test_functions.py
@@ -0,0 +1,9 @@
+"""Tests for functions"""
+
+import akshara.varnakaarya as vk
+
+def test_get_vinyaasa():
+    """Test get_vinyaasa() function"""
+
+    assert vk.get_vinyaasa('अरिन्दमः') == ['अ', 'र्', 'इ', 'न्', 'द्', 'अ', 'म्', 'अ', 'ः']
+    assert vk.get_vinyaasa('संस्कृतम्') == ['स्', 'अ', 'ं', 'स्', 'क्', 'ऋ', 'त्', 'अ', 'म्']
