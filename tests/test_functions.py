4a7d97e91e9b91f5d4dc0f36e4ef390984b68246
Arindam Saha <arindamsaha1507@gmail.com>
2023-03-15 05:42:22 +0000

    Changes by black

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,8 +2,29 @@
 
 import akshara.varnakaarya as vk
 
+
 def test_get_vinyaasa():
     """Test get_vinyaasa() function"""
 
-    assert vk.get_vinyaasa('अरिन्दमः') == ['अ', 'र्', 'इ', 'न्', 'द्', 'अ', 'म्', 'अ', 'ः']
-    assert vk.get_vinyaasa('संस्कृतम्') == ['स्', 'अ', 'ं', 'स्', 'क्', 'ऋ', 'त्', 'अ', 'म्']
+    assert vk.get_vinyaasa("अरिन्दमः") == [
+        "अ",
+        "र्",
+        "इ",
+        "न्",
+        "द्",
+        "अ",
+        "म्",
+        "अ",
+        "ः",
+    ]
+    assert vk.get_vinyaasa("संस्कृतम्") == [
+        "स्",
+        "अ",
+        "ं",
+        "स्",
+        "क्",
+        "ऋ",
+        "त्",
+        "अ",
+        "म्",
+    ]
