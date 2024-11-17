cc58720181ad04ad223f6b9b1e50368b1dbdeaa1
Arindam Saha <arindamsaha1507@gmail.com>
2024-11-17 15:32:04 +0000

    Added ashshara api

--- api.py
+++ api.py
@@ -3,6 +3,7 @@
 from fastapi import FastAPI
 
 from akshara import varnakaarya as vk
+from akshara.varna import Varnamaalaa
 
 app = FastAPI()
 
@@ -19,6 +20,33 @@ async def api_vinyaasa(word: str):
         status = "failure"
 
     return {"vinyaasa": vinyaasa, "status": status}
+
+
+@app.get("/akshara/{word}")
+async def api_akshara(word: str):
+    """Return the akshara of a word."""
+
+    try:
+        varnas = vk.get_vinyaasa(word)
+        status = "success"
+    except AssertionError:
+        akshara = None
+        status = "failure"
+
+    vn = Varnamaalaa()
+    # num_svaras = sum(1 for x in varnas if x in vn.svara)
+    index_svara = [i for i, x in enumerate(varnas) if x in vn.svara]
+
+    akshara = []
+    start = 0
+    for i in index_svara:
+        akshara.append(vk.get_shabda(varnas[start : i + 1]))
+        start = i + 1
+
+    if index_svara[-1] != len(varnas) - 1:
+        akshara[-1] = vk.get_shabda(varnas[index_svara[-2] + 1 :])
+
+    return {"akshara": akshara, "status": status}
 
 
 @app.get("/shabda/{letters}")
