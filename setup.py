9dd0432e59a5e9405115f4c14bcbb423faaeaaab
Arindam Saha <arindamsaha1507@gmail.com>
2024-11-22 18:47:02 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="akshara",
-    version="1.1.0",
+    version="1.1.1",
     author="Arindam Saha",
     author_email="arindamsaha1507@gmail.com",
     url="https://github.com/arindamsaha1507/akshara",
